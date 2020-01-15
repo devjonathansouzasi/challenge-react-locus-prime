04fe875506966badf3b090af2086c9b59938ee69
Jonathan Souza <devjonathansouzasi@gmail.com>
2020-01-15 13:57:12 -0300

    feat: adicionar dados dinamicos no header
    n

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { rootHistory } from "~/services/historys";
+import { AuthActions } from "~/store/ducks/auth";
 
 import {
 	Container,
@@ -9,12 +13,19 @@ import {
 } from "./styles";
 
 export default function Header() {
+	const auth = useSelector(state => state.auth);
+	const dispatch = useDispatch();
+	function handleLogout() {
+		dispatch(AuthActions.logout());
+		rootHistory.replace("/");
+	}
+
 	return (
 		<Container>
 			<HeaderTitle to="/">GIT PROFILES</HeaderTitle>
 			<UserInfoContainer>
-				<Name>Jonathan Souza</Name>
-				<LogoutButton>Sair</LogoutButton>
+				<Name>{auth.user.name}</Name>
+				<LogoutButton onClick={handleLogout}>Sair</LogoutButton>
 			</UserInfoContainer>
 		</Container>
 	);
